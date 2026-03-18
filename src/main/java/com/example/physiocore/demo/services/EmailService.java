4746af7dd708ad97b3268d14810f9f5e99eecd21
AlexCalafatG <acalafatg@gmail.com>
2026-03-18 18:00:37 +0100

    Mandamos el email en un html mas bonito

--- src/main/java/com/example/physiocore/demo/services/EmailService.java
+++ src/main/java/com/example/physiocore/demo/services/EmailService.java
@@ -1,21 +1,38 @@
 package com.example.physiocore.demo.services;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service("emailService")
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        mailSender.send(message);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true); // El true indica que es HTML
+
+            FileSystemResource file = new FileSystemResource(
+                    new File("src/main/resources/static/FisioCore-vertical.png"));
+
+            helper.addInline("logo", file);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Error al enviar el email", e);
+        }
     }
 }
